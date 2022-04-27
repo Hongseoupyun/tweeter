d7ed0184899d7aa13b797d0c20f1d3e4e59f0f8f
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-04-26 23:18:05 -0400

    fixed nav bar

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   console.log("jquery ready");
 
+  //access to text area and gets length of the input and updating the value and color of the counter
   $("#tweet-text").on("keyup", function () {
     let counter = $(this).parent().next().children()[1];
     let lengthOfText = $(this).val().length;
-    console.log(lengthOfText);
     $(counter).val(140 - lengthOfText);
     let newLength = $(counter).val();
     if (newLength < 0) {
