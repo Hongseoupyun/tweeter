8a6bb19ba4ad995b4019277de1fc65e776ad8836
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-04-27 19:59:13 -0400

    Ajax GET request added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,6 @@
 $(document).ready(function () {
   console.log("jquery ready at clinet.js");
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
+
   //returngin html template
   const createTweetElement = function (tweetObject) {
     const $tweet = $(`<article>
@@ -44,24 +21,39 @@ $(document).ready(function () {
     return $tweet;
   };
   //loops through the tweets and calls createTweetElement to append the result to ".tweet-container " class
-  const renderTweets = function (tweets) {
-    for (let tweet of tweets) {
+  const renderTweets = function (tweetsArray) {
+    for (let tweet of tweetsArray) {
       let result = createTweetElement(tweet);
       $(".tweet-container").prepend(result);
     }
   };
-  renderTweets(data);
-//Ajax post request from .tweetform
+  //Ajax POST request from .tweetform
   const $tweetForm = $(".tweetform");
 
   $tweetForm.submit(function (event) {
     event.preventDefault();
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:8080/tweets/",
+    $.ajax("http://localhost:8080/tweets/", {
+      method: "POST",
       data: $(this).serialize(),
     }).then(() => {
       console.log("Tweet Submitted!");
     });
   });
+  // Ajax GET request
+  const loadtweets = function () {
+    $.ajax("/tweets", { method: "GET", success: renderTweets }).then(() => {
+      console.log("GET success!");
+    });
+  };
+
+  loadtweets();
 });
+
+/*$.ajax({
+  type: "POST",
+  url: "http://localhost:8080/tweets/",
+  data: $(this).serialize(),
+}).then(() => {
+  console.log("Tweet Submitted!");
+});*/
+//$.get("/tweets", renderTweets)
