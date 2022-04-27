c02dc360bf71b4605cca2a771a4e2f455c7bada1
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-04-27 17:34:52 -0400

    Database hardcoded into client.js, template literals added and tweet-container content removed from index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,58 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+// Test / driver code (temporary). Eventually will get this from the server.
 
+$(document).ready(function () {
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+
+  const createTweetElement = function (tweetObject) {
+    const $tweet = $(`<article>
+    <header class="newtonheader">
+    <div class="username"><img src=${tweetObject.user.avatars}>
+    ${tweetObject.user.name}</div>
+    </div>
+    <div>${tweetObject.user.handle}</div>
+    </header>
+    <p id="tweet-text" >${tweetObject.content.text}</p>
+    <footer class="tweet-footer">
+    <div>${tweetObject.created_at}</div>
+    <div><i class="fa-solid fa-flag icon"></i><i class="fa-solid fa-retweet icon"></i><i
+    class="fa-solid fa-heart icon"></i>
+    </div>
+    </footer>
+    </article>`);
+    return $tweet;
+  };
+
+  const renderTweets = function (tweets) {
+    for (let tweet of tweets) {
+      let result = createTweetElement(tweet);
+      $(".tweet-container").prepend(result);
+    }
+  };
+  renderTweets(data);
+});
