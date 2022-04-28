5ccfcdd13cc1257300a0e9a807fc8c19fda25e34
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-04-28 16:33:24 -0400

    Escape function implemented and used on #tweet-text

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
   console.log("jquery ready at clinet.js");
-  //returngin html template
+  //returning html template
   const createTweetElement = function (tweetObject) {
     const $tweet = $(`<article class="tweet-container-item">
     <header class="newtonheader">
@@ -9,7 +14,7 @@ $(document).ready(function () {
     </div>
     <div>${tweetObject.user.handle}</div>
     </header>
-    <p id="tweet-text" >${tweetObject.content.text}</p>
+    <p id="tweet-text" >${escape(tweetObject.content.text)}</p>
     <footer class="tweet-footer">
     <div>${timeago.format(tweetObject.created_at)}</div>
     <div><i class="fa-solid fa-flag icon"></i><i class="fa-solid fa-retweet icon"></i><i
@@ -19,7 +24,6 @@ $(document).ready(function () {
     </article>`);
     return $tweet;
   };
-
 
   //loops through the tweets and calls createTweetElement to append the result to ".tweet-container " class
   const renderTweets = function (tweetsArray) {
@@ -32,39 +36,25 @@ $(document).ready(function () {
   const $tweetForm = $(".tweetform");
   $tweetForm.submit(function (event) {
     event.preventDefault();
-    const tweettext = $("#tweet-text").val()
-    console.log(tweettext)
+    const tweettext = $("#tweet-text").val();
     if (tweettext === null || tweettext === "") {
-      alert("Your tweet is empty!")
+      alert("Your tweet is empty!");
     } else if (tweettext.length > 140) {
-      alert("Your tweet is too long!")
+      alert("Your tweet is too long!");
     } else {
       $.ajax("http://localhost:8080/tweets/", {
         method: "POST",
         data: $(this).serialize(),
       }).then(() => {
         console.log("Tweet Submitted!", $(this).serialize());
+        loadtweets();
       });
     }
   });
-
   // Ajax GET request
   const loadtweets = function () {
     $.ajax("/tweets", { method: "GET", success: renderTweets }).then(() => {
       console.log("GET success!");
     });
   };
-
-  loadtweets();
-
-
 });
-
-/*$.ajax({
-  type: "POST",
-  url: "http://localhost:8080/tweets/",
-  data: $(this).serialize(),
-}).then(() => {
-  console.log("Tweet Submitted!");
-});*/
-//$.get("/tweets", renderTweets)
